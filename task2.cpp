73e664be503516ee079437beee1f3c520ec3fa9e
Kirill <1altumsilentium1@gmail.com>
2022-12-04 17:39:29 +0300

    Second task

---
+++ task2.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<string>
+#include<Windows.h>
+struct Account
+{
+	int account_number = 0;
+	std::string name;
+	double balance = 0;
+};
+void Change(Account* balance)
+{
+	std::cout << "Введите новый балансе: ";
+	std::cin >> balance->balance;
+}
+
+int main(int argc, char** argv)
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	std::cout << "Введите номер счёта: ";
+	Account num;
+	std::cin >> num.account_number;
+	std::cout << "Введите имя владельца: ";
+	Account name;
+	std::cin >> name.name;
+	std::cout << "Введите баланс: ";
+	Account balance;
+	std::cin >> balance.balance;
+	Change(&balance);
+	std::cout << "Ваш счёт: " << name.name << " " << num.account_number << " " << balance.balance << std::endl;
+	return 0;
+}
